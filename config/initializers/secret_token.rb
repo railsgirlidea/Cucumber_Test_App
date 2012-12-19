3f2c3c64131b66d16285ba18337a0cc9a43be5e6
kalpana inkar <kalpana.inkar@gmail.com>
2012-12-19 14:50:41 +0530

    code push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cucumbertest::Application.config.secret_token = 'eddc177efa6ee93ad656cf4fee64c07c7118d50a571e3084978a8cf270f6aed53133f3d452358d2a77bdb2d97b701d35bcba3697bb91bb83b94363703869d782'
